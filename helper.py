cabec31311ae788d8bad6697f774c9afa8422e78
Wensheng Zhang <wensheng.zhang@rwth-aachen.de>
2024-07-20 15:54:57 +0200

    Add time calculation

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-
+import time
+import math
 
 def visualize_graph(G, color):
     plt.figure(figsize=(7,7))
@@ -9,3 +10,10 @@ def visualize_graph(G, color):
     nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=False,
                      node_color=color, cmap="Set2")
     plt.show()
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
