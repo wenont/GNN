d213315847f3a62a3120c2471f801d4baefe2c36
Wensheng Zhang <wensheng.zhang@rwth-aachen.de>
2024-07-24 18:41:50 +0200

    Improve console print, can get the best result now

--- my_gnn.py
+++ my_gnn.py
@@ -11,16 +11,16 @@ def run(dataset_name, hidden_channels=64, num_epochs=64, batch_size=200):
     root_name = f'data/TUDataset/{dataset_name}'
     dataset = TUDataset(root=root_name, name=dataset_name, use_node_attr=True)
     dataset = dataset.shuffle()
-    centered_line = f'Dataset: {dataset}'.center(80-2)
-    print('=' * 80 + f'\n={centered_line}=\n' + '=' * 80 + '\n')
+    centered_line = f'Dataset: {dataset}'.center(93)
+    print('=' * 93 + f'\n{centered_line}\n' + '=' * 93 + '\n')
 
 
     train_dataset_size = int(len(dataset) * 0.8)
     train_dataset = dataset[:train_dataset_size]
-    test_dataset = dataset[train_dataset_size:]
+    val_dataset = dataset[train_dataset_size:]
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
     # Define model
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -51,6 +51,17 @@ def run(dataset_name, hidden_channels=64, num_epochs=64, batch_size=200):
     @torch.no_grad()
     def val(loader):
         model.eval()
+        loss_all = 0
+        for data in loader:
+            data = data.to(device)
+            out = model(data.x, data.edge_index, data.batch)
+            loss = criterion(out, data.y)
+            loss_all += loss.item() * data.num_graphs
+        return loss_all/len(loader.dataset)
+    
+    @torch.no_grad()
+    def test(loader):
+        model.eval()
         correct = 0
         for data in loader:
             data = data.to(device)
@@ -59,32 +70,43 @@ def run(dataset_name, hidden_channels=64, num_epochs=64, batch_size=200):
         return correct / len(loader.dataset)
 
 
-    test_acc = val(test_loader)
-    print(f'Initial test accuracy: {test_acc:.4f}')
+    val_loss = val(val_loader)
+    val_acc = test(val_loader)
+    print(f'Initial val loss: {val_loss:.4f}, Initial val accuracy: {val_acc:.4f}')
 
     # Train model
+    train_losses = []
+    val_losses = []
     train_accs = []
-    test_accs = []
-    losses = []
+    val_accs = []
+
+    best_val_loss, best_val_acc = float('inf'), 0
+
     start = time.time()
     for epoch in range(num_epochs+1):
-        loss = train(train_loader)
-        train_acc = val(train_loader)
-        test_acc = val(test_loader)
+        train_loss = train(train_loader)
+        val_loss = val(train_loader)
+        train_acc = test(train_loader)
+        val_acc = test(val_loader)
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_val_acc = val_acc
         if epoch % 10 == 0:
-            test_accs.append(test_acc)
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+            val_accs.append(val_acc)
             train_accs.append(train_acc)
-            losses.append(loss)
-            print(f'Epoch: {epoch:03d} ({timeSince(start)}), Loss: {loss:.4f}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
+            print(f'Epoch: {epoch:03d} ({timeSince(start)}), Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Train Acc: {train_acc:.4f}, Val Acc: {val_acc:.4f}')
 
+    print(f'Best validation loss on {dataset_name}: {best_val_loss:.4f} with the accuracy: {best_val_acc:.4f}\n')
     # Plot results
     netParams = NetParams(hidden_channels, num_epochs, batch_size)
-    plot_training_results(dataset_name, netParams, test_accs, train_accs, losses)
+    plot_training_results(dataset_name, netParams, train_accs, val_accs, train_losses, val_losses)
 
 if __name__ == '__main__':
     hidden_channels = 64
     batch_size = 64
     num_epochs = 200
-    dataset_name = 'DD'
+    dataset_name = 'ER_MD'
 
     run(dataset_name, hidden_channels, num_epochs, batch_size)
