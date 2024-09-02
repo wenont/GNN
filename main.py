a82fdb6f7b7b222eb392a5441dcd5e337dfe2a36
Wensheng Zhang <wenhseng.zhang@rwth-aachen.de>
2024-09-02 11:20:41 +0200

    update main py

--- main.py
+++ main.py
@@ -1,34 +1,87 @@
+from train_procedure import get_generalization_error_from_a_dataset
 import pandas as pd
+import logging
+from tabulate import tabulate
+from utils import get_average_degree, get_average_shortest_path, read_file_to_list
+from tqdm import tqdm
+import logging
 import matplotlib.pyplot as plt
 
-df1 = pd.DataFrame({
-    'Name': ['FRANKENSTEIN', 'NCI1', 'COIL-RAG', 'Letter-high', 'DD', 'PROTEINS_full', 'COLORS-3'],
-    'Ave. generalization error': [-0.0015, 0.0479, -0.0043, 0.0513, 0.1483, -0.0213, 0.0090]
-})
 
-df2 = pd.DataFrame({
-    'Name': ['FRANKENSTEIN', 'NCI1', 'COIL-RAG', 'Letter-high', 'DD', 'PROTEINS_full', 'COLORS-3'],
-    'Ave. degree': [2.0627741588132293, 2.1550138021472596, 1.8277218368573067, 1.8896836075252956, 4.979061666490266, 3.7346421664401634, 2.928853359699249],
-    'Ave. shortest path': [3.6521139346083733, 5.469777618361717, 1.0606131260794474, 1.5173343777117363, 7.970076470113258, 4.712380854361992, 2.0945741756348464]
-})
+def calculate_generalation_error():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='./results/parameters.log', encoding='utf-8', level=logging.INFO, format='%(message)s', filemode='a')
+    datasets = read_file_to_list('datasets.txt')
+    
+    df = pd.DataFrame({
+        'Name': [],
+        'Ave. generalization error': [],
+        'std': []
+    })
+    
+    for dataset in datasets:
+        generalization_error, std = get_generalization_error_from_a_dataset(dataset)
+        df.loc[len(df)] = [dataset, generalization_error, std]
+    
+    logger.info(tabulate(df, headers='keys', tablefmt='psql'))
+    df.to_csv('generalization_error.csv')
 
-df_combined = pd.merge(df1, df2, on='Name')
+def calcualte_parameters():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='./results/parameters.log', encoding='utf-8', level=logging.INFO, format='%(message)s', filemode='a')
+    datasets = read_file_to_list('datasets.txt')
 
-df_sorted_by_degree = df_combined.sort_values('Ave. degree')
-plt.figure(figsize=(10, 5))
+    df = pd.DataFrame({
+        'Name': [],
+        'Ave. degree': [],
+        'Ave. shortest path': [],
+    })
 
-plt.subplot(1, 2, 1)
-plt.plot(df_sorted_by_degree['Ave. degree'], df_sorted_by_degree['Ave. generalization error'], marker='o', color='b')
-plt.title('Ave. Degree vs. Ave. Generalization Error')
-plt.xlabel('Ave. Degree')
-plt.ylabel('Ave. Generalization Error')
+    for dataset in tqdm(datasets):
+        df.loc[len(df)] = [dataset, get_average_degree(dataset), get_average_shortest_path(dataset)]
 
-df_sorted_by_shortest_path = df_combined.sort_values('Ave. shortest path')
-plt.subplot(1, 2, 2)
-plt.plot(df_sorted_by_shortest_path['Ave. shortest path'], df_sorted_by_shortest_path['Ave. generalization error'], marker='o', color='r')
-plt.title('Ave. Shortest Path vs. Ave. Generalization Error')
-plt.xlabel('Ave. Shortest Path')
-plt.ylabel('Ave. Generalization Error')
+    logger.info(tabulate(df, headers='keys', tablefmt='psql'))
+    df.to_csv('parameters.csv')
 
-plt.tight_layout()
-plt.show()
+
+def compare_generalization_error_and_parameters():
+
+    df1 = pd.read_csv('results/generalization_error.csv')
+    df2 = pd.read_csv('results/parameters.csv')
+
+    df_combined = pd.merge(df1, df2, on='Name')
+
+    df_sorted_by_degree = df_combined.sort_values('Ave. degree')
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(df_sorted_by_degree['Ave. degree'], df_sorted_by_degree['Ave. generalization error'], marker='o', color='b')
+    plt.title('Ave. Degree vs. Ave. Generalization Error')
+    plt.xlabel('Ave. Degree')
+    plt.ylabel('Ave. Generalization Error')
+
+    df_sorted_by_shortest_path = df_combined.sort_values('Ave. shortest path')
+    plt.subplot(1, 2, 2)
+    plt.plot(df_sorted_by_shortest_path['Ave. shortest path'], df_sorted_by_shortest_path['Ave. generalization error'], marker='o', color='r')
+    plt.title('Ave. Shortest Path vs. Ave. Generalization Error')
+    plt.xlabel('Ave. Shortest Path')
+    plt.ylabel('Ave. Generalization Error')
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    # get input from user
+    print('Please choose from the following options:')
+    print('1. Get generalization error')
+    print('2. Get Parameters')
+    print('3. Compare generalization error and parameters')
+    print('4. Exit')
+    option = input('Enter your choice: ')
+
+    if option == '1':
+        calculate_generalation_error()
+    elif option == '2':
+        calcualte_parameters()
+    elif option == '3':
+        compare_generalization_error_and_parameters()
