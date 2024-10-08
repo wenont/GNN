3851b8f490eb69cc96a04c0ad34555d829ee24c6
Wensheng Zhang <wenhseng.zhang@rwth-aachen.de>
2024-10-08 18:16:48 +0200

    Add graph analysis functions to calculate various graph metrics

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import math
 from dataclasses import dataclass
 from torch_geometric.datasets import TUDataset
 from tqdm import tqdm
+import torch
 
 
 
@@ -90,6 +91,156 @@ def get_average_shortest_path(dataset_name, show_errors=False):
         print(f'Error rate: {num_errors / len(dataset)}')
             
     return sum(avg_shortest_paths) / len(avg_shortest_paths)
+
+def get_graph_diameter(dataset_name, show_errors=False):
+    '''
+    Calculate the diameter of the graph
+    '''
+    dataset = TUDataset(root='data/TUDataset', name=dataset_name, use_node_attr=True)
+    diameters = []
+    num_errors = 0
+
+    for data in dataset:
+        G = to_networkx(data)
+        try:
+            diameters.append(nx.diameter(G))
+        except:
+            num_errors += 1
+            continue
+    if show_errors:
+        print(f'Number of errors: {num_errors}')
+        print(f'Error rate: {num_errors / len(dataset)}')
+
+    return sum(diameters) / len(diameters)
+
+def get_graph_density(dataset_name):
+    '''
+    Calculate the density of the graph
+    '''
+    dataset = TUDataset(root='data/TUDataset', name=dataset_name, use_node_attr=True)
+    densities = []
+
+    for data in dataset:
+        G = to_networkx(data)
+        densities.append(nx.density(G))
+
+    return sum(densities) / len(densities)
+
+def get_graph_clustering_coefficient(dataset_name):
+    '''
+    Calculate the clustering coefficient of the graph
+    '''
+    dataset = TUDataset(root='data/TUDataset', name=dataset_name, use_node_attr=True)
+    clustering_coefficients = []
+
+    for data in dataset:
+        G = to_networkx(data)
+        clustering_coefficients.append(nx.average_clustering(G))
+
+    return sum(clustering_coefficients) / len(clustering_coefficients)
+
+def get_graph_transitivity(dataset_name):
+    '''
+    Calculate the transitivity of the graph
+    '''
+    dataset = TUDataset(root='data/TUDataset', name=dataset_name, use_node_attr=True)
+    transitivity = []
+
+    for data in dataset:
+        G = to_networkx(data)
+        transitivity.append(nx.transitivity(G))
+
+    return sum(transitivity) / len(transitivity)
+
+def get_graph_assortativity(dataset_name):
+    '''
+    Calculate the assortativity of the graph
+    '''
+    dataset = TUDataset(root='data/TUDataset', name=dataset_name, use_node_attr=True)
+    assortativity = []
+
+    for data in dataset:
+        G = to_networkx(data)
+        assortativity.append(nx.degree_assortativity_coefficient(G))
+
+    return sum(assortativity) / len(assortativity)
+
+def get_average_closeness_centrality(dataset_name):
+    '''
+    Calculate the average closeness centrality of the graph
+    '''
+    dataset = TUDataset(root='data/TUDataset', name=dataset_name, use_node_attr=True)
+    closeness_centralities = []
+
+    for data in dataset:
+        G = to_networkx(data)
+        closeness_centralities.append(sum(nx.closeness_centrality(G).values()) / G.number_of_nodes())
+
+    return sum(closeness_centralities) / len(closeness_centralities)
+
+def get_average_betweenness_centrality(dataset_name):
+    '''
+    Calculate the average betweenness centrality of the graph
+    '''
+    dataset = TUDataset(root='data/TUDataset', name=dataset_name, use_node_attr=True)
+    betweenness_centralities = []
+
+    for data in dataset:
+        G = to_networkx(data)
+        betweenness_centralities.append(sum(nx.betweenness_centrality(G).values()) / G.number_of_nodes())
+
+    return sum(betweenness_centralities) / len(betweenness_centralities)
+
+def get_average_eigenvector_centrality(dataset_name):
+    '''
+    Calculate the average eigenvector centrality of the graph
+    '''
+    dataset = TUDataset(root='data/TUDataset', name=dataset_name, use_node_attr=True)
+    eigenvector_centralities = []
+
+    for data in dataset:
+        G = to_networkx(data)
+        ec = nx.eigenvector_centrality(G)
+        print(ec)
+        eigenvector_centralities.append(sum(nx.eigenvector_centrality(G).values()) / G.number_of_nodes())
+
+    return sum(eigenvector_centralities) / len(eigenvector_centralities)
+
+def wl_1d_color_count(dataset_name):
+    '''
+    Calculate the average number of coloring in 1WL of the graph
+    '''
+    dataset = TUDataset(root='data/TUDataset', name=dataset_name, use_node_attr=True)
+    color_count_sum = []
+    
+    for data in dataset:
+        Graph = to_networkx(data)
+        # Initialize all nodes with the same color
+        colors = {node: 0 for node in Graph.nodes()}
+        stable = False
+        
+        # Perform color refinement
+        while not stable:
+            new_colors = {}
+            for node in Graph.nodes():
+                neighbor_colors = sorted([colors[neighbor] for neighbor in Graph.neighbors(node)])
+                new_colors[node] = f"{colors[node]}|{'|'.join(map(str, neighbor_colors))}"            
+            label_to_color = {}
+            for new_label in new_colors.values():
+                if new_label not in label_to_color:
+                    label_to_color[new_label] = len(label_to_color)
+            
+            # Apply the new coloring
+            new_colors = {node: label_to_color[new_colors[node]] for node in Graph.nodes()}
+            
+            # Check for stability
+            if colors == new_colors:
+                stable = True
+            colors = new_colors
+        
+        color_count_sum.append(len(set(colors.values())))
+    
+    return sum(color_count_sum) / len(color_count_sum)
 
 def read_file_to_list(file_path):
     """
