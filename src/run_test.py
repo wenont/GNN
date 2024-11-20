f9955f535ae08fbc7b1cdb9ad160ec558c81cdc7
Wensheng Zhang <wensheng.zhang@rwth-aachen.de>
2024-11-20 22:45:49 +0100

    Implement dataset processing in run_test.py with error handling for train_test function

--- src/run_test.py
+++ src/run_test.py
@@ -1,6 +1,28 @@
-from train import hyperparameter_tuning
+from ast import mod
+from os import error
 import wandb
+from utils import read_file_to_list
+from train import train_test
+import os.path as osp
+import pandas as pd
 
 
-sweep_id = 's4v4a4ij'
-wandb.agent(sweep_id, hyperparameter_tuning, count=1000, project='bt')
+
+model_name = 'MPNN'
+path = osp.join(osp.dirname(__file__), '..', 'data', 'test_dataset.txt')
+dataset_list = read_file_to_list(path)
+
+success_list = []  
+error_list = []
+
+for dataset_name in dataset_list:
+    try:
+        train_test(dataset_name, model_name)
+        success_list.append(dataset_name)
+    except Exception as e:
+        print(f"Error in {dataset_name}: {e}")
+        error_list.append(dataset_name)
+        continue
+
+print(f"Success: {success_list}")
+print(f"Error: {error_list}")
