54b94399733c0bb96c0a079733a5d15932956ae2
Wensheng Zhang <wensheng.zhang@rwth-aachen.de>
2024-12-17 08:04:09 +0100

    Add temporary flag support in setup_sweep.py for sweep creation

--- src/setup_sweep.py
+++ src/setup_sweep.py
@@ -1,7 +1,13 @@
 from utils import read_file_to_list, setup_wandb_sweep
 import os.path as osp
 import pandas as pd
+import argparse
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--temporory", help="set a temporary flag",
+                    action="store_true")
+args = parser.parse_args()
 
 project_name = 'bt_GCN'
 path = osp.join(osp.dirname(__file__), '..', 'data', 'test_dataset.txt')
@@ -14,12 +20,15 @@ for dataset_name in dataset_list:
 
 	# check if the df contains a row with the name of the dataset
 	print('Checking if the sweep exists')
-	if df[(df['name'] == dataset_name) & (df['project'] == project_name)].empty:
+	if df[(df['name'] == dataset_name) & (df['project'] == project_name)].empty | args.temporory:
 		print('Creating sweep')
-		sweep_id = setup_wandb_sweep(project_name, dataset_name)
+		if args.temporory:
+			sweep_id = setup_wandb_sweep(project_name, dataset_name, temp=True)
+		else:
+			sweep_id = setup_wandb_sweep(project_name, dataset_name)
 		print(f'Sweep id: {sweep_id}')
 		# save the dataset name, sweep_id and project name to the csv
-		df = pd.concat([df, pd.DataFrame([{'name': dataset_name, 'project': project_name, 'sweep_id': sweep_id}])], ignore_index=True)
+		df = pd.concat([df, pd.DataFrame([{'name': f'{dataset_name}_temp', 'project': project_name, 'sweep_id': sweep_id}])], ignore_index=True)
 		df.to_csv(path, index=False)
 	else:
 		print('Dataset found')
